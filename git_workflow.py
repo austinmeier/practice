09080a1cb30cbf2ee7397e8e41a99f24e4f647ec
austinmeier <meiera@science.oregonstate.edu>
2015-07-22 10:38:52 -0700

    added git_workflow.py

---
+++ git_workflow.py
@@ -0,0 +1,12 @@
+Created new repository
+"Practice"
+
+copied the url from middle right side:
+
+https://github.com/austinmeier/practice.git
+
+#typed in:
+$git clone https://github.com/austinmeier/practice.git
+
+#this downloads the repository
+
